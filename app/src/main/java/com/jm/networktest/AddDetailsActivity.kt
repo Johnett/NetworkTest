814d5ac17244895279d27f8ef71ec510b0159857
Johnett <johnettmathew@gmail.com>
2019-12-31 15:32:38 +0530

    Initial Commit: Lots of update(even though gradle is intact no new dependencies added yet)

---
+++ app/src/main/java/com/jm/networktest/AddDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.jm.networktest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddDetailsActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_add_details)
+
+  }
+}
